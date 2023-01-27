da13d3e6abcfa2955eb75da6781ca2dcb41a31df
alwong18 <121971634+alwong18@users.noreply.github.com>
2023-01-27 15:42:01 -0800

    Tried doing Challenge
    Making balls fall from top of the screen

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -6,9 +6,9 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22;
+    private float spawnLimitXLeft = 0;
     private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+    private float spawnPosZ = 30;
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
@@ -23,7 +23,7 @@ public class SpawnManagerX : MonoBehaviour
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), 0, spawnPosZ);
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
